ca50a2c9faab30385e4177c279a918fe863da49c
Uttam7470 <patidaruttam74@gmail.com>
2024-10-19 20:35:05 +0530

    deploy successfully

--- backend/index.js
+++ backend/index.js
@@ -80,7 +80,7 @@ app.use(cookieParser());
 
 // CORS configuration
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://twitter-ya90.onrender.com',
     credentials: true,
 };
 
